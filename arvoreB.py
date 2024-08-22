ce14f7614916b848011fd3c69ed769effb3b2240
unknown <fabriciraposo@gmail.com>
2024-08-21 21:09:51 -0300

    passar pro pc dnv

--- arvoreB.py
+++ arvoreB.py
@@ -1,47 +1,80 @@
 import sys
 import os
 import struct
-#from dataclasses import dataclass, field
 
-#variaveis globais
+#ordem da arvore
+ORDEM = 5
+
+#tamanho do registro baseado na ordem
+tamRegbtree = (12*ORDEM)-4
+
+#nome dos arquivos
 arqG = "games.dat"
-arvoB = "btree.dat"
-ORDEM = 4
-arquivo = {} 
+arqB = "btree.dat" 
+
+#RRN da raiz
+rrnRais = -1
 
 #@dataclass
-class Pagina:
+class paginaAvore:
     def __init__(self) -> None:
         self.numChaves: int = 0
-        self.chaves: list = [None] * (ORDEM-1)
-        self.filhos: list = [None] * ORDEM
+        self.chaves: int = [-1] * (ORDEM-1)
+        self.offsetFilhos: int = [-1]*(ORDEM-1)
+        self.filhos: int = [-1] * ORDEM
+def lePagina(RRN:int) -> None:
+    offset = 4 + (RRN * tamRegbtree)
+    pag = paginaAvore()
+    with open(arqB, "rb") as arq:
+        arq.seek(offset)
+        pag.numChaves = struct.unpack("<i", arq.read(4))[0]
+        for i in range(ORDEM-1):
+            pag.chaves[i] = struct.unpack(">i",arq.read(4))[0]
+        for i in range(ORDEM-1):
+            pag.offsetFilhos[i] = struct.unpack(">i",arq.read(4))[0]
+        for i in range(ORDEM-1):
+            pag.filhos[i] = struct.unpack(">i", arq.read(4)[0])
+    return pag  
 
-    def estaCheia(self):
-        return self.numChaves >= ORDEM - 1
-
-def leOperacoes(nomeArqOP : str):
-    arqOP = open(nomeArqOP, "r")
-    linha = arqOP.readline()
-    while linha:
-        char = linha.split(sep=" ")
-        if char[0] == "c":
-            #cria btree
-        elif char[0] == "b":
-            print("foda")
-
-def buscaNaArvore(chave, rrn):
-    if rrn == None:
-        return False, None, None
+def escrevePagina(RRN:int, pag:paginaAvore): #escreve a pagina no arquivo da arvoreB
+    if RRN == -1:
+        offset = 4
     else:
-        pag = rrn
+        offset = 4 + (RRN * tamRegbtree)
+        with open(arqB, "rb") as arq:
+            arq.seek(offset)
+            arq.write(struct.pack(">i", pag.numChaves))
+            for i in range(ORDEM-1):
+                print(pag.chave[i])
+                arq.write(struct.pack(">i", pag.chaves[i]))
+            for i in range(ORDEM-1):
+                arq.write(struct.pack(">i", pag.offsetFilhos[i]))
+            for i in range(ORDEM-1):
+                arq.write(struct.pack(">i", pag.filhos))
+
+def insereNaPagina(chave, filhoD, pag:paginaAvore):
+    if pag.numChaves == (ORDEM-1):
+        pag.filhos.append(None)
+        pag.chaves.append(None)
+    i = pag.numChaves
+    while i > 0 and chave < pag.chaves [i-1]:
+        
+def buscaNaArvore(chave: int, rrn: int):
+    if rrn == -1:
+        return False, None, None
+    
+    else:
+        pag = lePagina(rrn)
         achou, pos = buscaNaPagina(chave,rrn)
+
         if achou:
             return True, rrn, pos
+        
         else:
             return buscaNaArvore(chave, pag.filhos[pos])
 
 def buscaNaPagina(chave, pag):
-    pos = 0
+    pos:int = 0
     while pos < pag.numChaves and chave > pag.chaves[pos]:
         pos += 1
     if pos < pag.numChaves and chave == pag.chaves[pos]:
@@ -50,42 +83,31 @@ def buscaNaPagina(chave, pag):
         return False, pos
     
 def insereNaArvore(chave, rrnAtual):
-    if rrnAtual == None:
+    if rrnAtual == -1:
         chavePro = chave
-        filhoDpro = None
-        return chavePro, filhoDpro
+        filhoDpro = -1
+        return chavePro, filhoDpro, True
     else:
-        pag = rrnAtual
+        pag = lePagina(rrnAtual)
         achou, pos = buscaNaArvore(chave, pag)
     
     if achou:
-        print("erro chave duplicada")#da pra gerar um erro tbm fe
-
+        print("erro! chave duplicada")#da pra gerar um erro tbm fe
+        raise(ValueError)
+    
     chavePro, filhoDpro, promo = insereNaArvore(chave, pag.filhos[pos])
     if not promo:
-        return None, None, False
+        return -1, -1, False
     else:
-        if 
+        if pag.numChves != (ORDEM-1):
+            insereNaPagina(chavePro, filhoDpro, pag)
+            escrevePagina(rrnAtual, pag)
+            return -1, -1, False
 
         else:
             chavePro, filhoDpro, pag, novapag = divide(chavePro, filhoDpro, pag)
-        arq.seek(rrnAtual)
-        arq.write(pag)
-        arq.flush        
+            escrevePagina(rrnAtual, pag)
+            escrevePagina(novapag, filhoDpro)
+            return chavePro, filhoDpro, True
 
 def divide():
-
-try:
-    arq = open(arqG, 'rb')
-    arq.close()
-except:
-    print(f"arquivo {arqG} não existe!")
-    exit(0)
-
-if sys.argv[1] == "-c":
-    leOperacoes(sys.argv[2])
-
-elif sys.argv[1] == "-e":
-    print("teste")
-elif sys.argv[1] == "-p":
-    print('teste')
